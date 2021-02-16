b466382d9e84626a481e45b9b4e389600412fa14
kmodipa <kmodipa@student.42.fr>
2021-02-16 08:53:24 +0200

    login modal

--- Front-End/The-Movie-Database-Project/src/app/Components/common/navbar/navbar.component.ts
+++ Front-End/The-Movie-Database-Project/src/app/Components/common/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import {ModalService} from '../../_modal';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
@@ -10,20 +12,39 @@ export class NavbarComponent implements OnInit {
 
   isLogIn: boolean;
   searchValue: any;
+  stageForm: FormGroup;
 
   constructor(private router: Router,
-              public activatedRoute: ActivatedRoute) { }
+              public activatedRoute: ActivatedRoute,
+              public modalService: ModalService,
+              public formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.isLogIn = true;
+    this.initiateForm();
   }
 
   logIn(): void {
 
   }
 
+  onOut(): void {
+    this.modalService.close('login');
+  }
+
+  goToContact(): void {
+    this.router.navigate(['contact-us']);
+  }
+
   logOut(): void {
 
+  }
+
+  initiateForm(): void {
+    this.stageForm = this.formBuilder.group({
+      username: '',
+      password: ''
+    });
   }
 
   search(phrase: string): void {
