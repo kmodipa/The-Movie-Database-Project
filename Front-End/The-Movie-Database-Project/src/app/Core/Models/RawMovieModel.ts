056828c93019343cabb6c363b4ca6403fbbce89a
kmodipa <kmodipa@student.42.fr>
2021-02-16 08:53:08 +0200

    custom modal

--- Front-End/The-Movie-Database-Project/src/app/Core/Models/RawMovieModel.ts
+++ Front-End/The-Movie-Database-Project/src/app/Core/Models/RawMovieModel.ts
@@ -1,13 +1,15 @@
 import { NumberValueAccessor } from "@angular/forms";
 
 export class MovieModel {
+  page: number;
+  results: {
     adult: string;
     backdropt_path: string;
     belongs_to_collection: {
-        id: number;
-        name: string;
-        poster_path: string;
-        backdrop_path: string;
+      id: number;
+      name: string;
+      poster_path: string;
+      backdrop_path: string;
     };
     budget: number;
     genders: {id: number, name: string}[];
@@ -20,22 +22,22 @@ export class MovieModel {
     popularity: number;
     poster_path: string;
     production_companies: {
-        id: number;
-        logo_path: null;
-        name: string;
-        origin_country: string;
+      id: number;
+      logo_path: null;
+      name: string;
+      origin_country: string;
     }[];
     production_countries: {
-        iso_3166_1: string;
-        name: string;
+      iso_3166_1: string;
+      name: string;
     }[];
     release_date: string;
     revenue: number;
     runtime: number;
     spoken_languages: {
-        english_name: string;
-        iso_639_1: string;
-        name: string;
+      english_name: string;
+      iso_639_1: string;
+      name: string;
     }[];
     status: string;
     tagline: string;
@@ -43,5 +45,8 @@ export class MovieModel {
     video: boolean;
     vote_average: number;
     vote_count: number;
+  }[];
+  total_results: number;
+  total_pages: number;
 }
 
