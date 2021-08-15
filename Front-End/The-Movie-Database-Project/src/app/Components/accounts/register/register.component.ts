bc35ef6a9675abca38e681faf8c3deb563cc0f4e
Khomotjo <kmodipa@student.42.fr>
2021-08-15 15:18:18 +0200

    login+register form-validation, contact-me+nav update,

--- Front-End/The-Movie-Database-Project/src/app/Components/accounts/register/register.component.ts
+++ Front-End/The-Movie-Database-Project/src/app/Components/accounts/register/register.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AccountsServiceService} from '../../../Services/AccountsService/accounts-service.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ModalService} from '../../_modal';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {UserModel} from '../../../Core/Models/UserModel';
 
 @Component({
@@ -29,29 +29,46 @@ export class RegisterComponent implements OnInit {
 
   initiateForm(): void {
     this.registerForm = this.formBuilder.group({
-      username: '',
-      email: '',
-      password: ''
+      email: ['',
+        [Validators.required,
+          Validators.pattern('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$')]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
 
   register(): void {
-    this.accountsService.register(this.userModel).subscribe(res => {
-      console.log(res);
-      if (res.status === 200) {
-        window.location.reload();
-      } else {
+    /* -Mark form as touched- */
+    this.email.markAsTouched();
+    this.password.markAsTouched();
 
-      }
+    if (this.registerForm.valid) {
+      this.accountsService.register(this.userModel).subscribe(res => {
+        console.log(res);
+        if (res.status === 200) {
+          window.location.reload();
+        } else {
 
-    }, error => {
-      console.log(error);
-    });
+        }
+
+      }, error => {
+        console.log(error);
+      });
+    }
+
   }
 
   openLogin(): void {
     this.modalService.close('register');
     this.modalService.open('login');
+  }
+
+  /* -registerForm Getters- */
+  get email(): AbstractControl {
+    return this.registerForm.get('email');
+  }
+
+  get password(): AbstractControl {
+    return this.registerForm.get('password');
   }
 
 }
